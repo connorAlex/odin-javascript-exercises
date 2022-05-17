42fc330397615acf7c24244ab95c8897a9fb0d7c
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-16 20:41:06 -0500

    complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(userString, count) {
+    let output = "";
+    
+    if (count < 0){
+        return 'ERROR'
+    }
 
+    for (i = 0; i < count; i++){
+        output = output + userString
+    }
+    return output;
 };
 
 // Do not edit below this line
