cc5218e97f54ec3d4888427ee4903074e97d8650
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-18 16:33:59 -0500

    brought up to date

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArr) {
+    let args = Array.from(arguments);
+
+    for (i = 1; i < args.length; i ++){
+        
+        let index = myArr.indexOf(args[i]);
+        
+        if (typeof(index) != 'undefined' && index >= 0){
+            myArr.splice(index,1);
+        }
+            
+    }
+    return myArr;
 
 };
+
+console.log(removeFromArray([1,2,3,4],8))
 
 // Do not edit below this line
 module.exports = removeFromArray;
