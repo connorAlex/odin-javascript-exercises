7b931d71d74ff514143b4f4b0db19edb6a39e4af
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-18 17:11:30 -0500

    add 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let minNum = 0;
+    let maxNum = 0;
+    if (typeof(x) != 'number' || typeof(y) != 'number' || x < 0 || y < 0){
+        return "ERROR";
+    }
+    if (Math.min(x,y) === x){
+        minNum = x;
+        maxNum = y;
+    }
+    else{
+        minNum = y;
+        maxNum = x;
+    }
+    let output = 0;
+    for (i = minNum; i <= maxNum; i++){
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
