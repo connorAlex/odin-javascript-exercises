8eefebd2b65d02d0448e88bb486ba766d36436fc
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-18 17:34:26 -0500

    add 07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ctof = function(f) {
+  //multiply by (9/5) and add in 32.
+  if (typeof(f) != 'number'){
+    return 'Error';
+  }
+  return Math.round((f * (9/5) + 32) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ftoc = function(c) {
+  //subtract 32 and multiply by 5/9
+  if (typeof(c) != 'number'){
+    return 'Error';
+  }
+  return Math.round((c - 32) * (5/9) * 10) / 10;
 };
 
 // Do not edit below this line
