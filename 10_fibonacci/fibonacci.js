640957f6d69c13ffa5ed25e47e5ada594a76c312
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-23 10:25:24 -0500

    added 10 fibbonaci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0){
+        return 'OOPS';
+    }
+    else if(typeof(x) === 'string'){
+        x = parseInt(x);
+    }
+    let fib = [];
+    for (i = 0; i < x; i++){
+        if (fib.length < 2){
+            fib.push(1);
+        }
+        else{
+            let a = fib[fib.length-1] + fib[fib.length-2];
+            fib.push(a);
+        }
+    }
+    return fib[x-1];
 };
 
 // Do not edit below this line
