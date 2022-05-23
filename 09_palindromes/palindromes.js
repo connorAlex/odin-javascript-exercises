8fe1acc19f2c59082ee35acdb468dc4beaac66e9
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-23 10:05:59 -0500

    finished 09 palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    a = a.toLowerCase();
+    let output = "";
+    const regex = /[^A-Za-z]/g;
+    b = a.replace(regex,"");
+    console.log("b:"+ b);
+    for (i = b.length - 1; i >= 0; i--){
+        output += b[i];
+    }
+    console.log(output);
+    if (output === b){
+        return true;
+    }
+    else{
+        
+        return false;
+    }
 };
-
+palindromes("hannah!!");
 // Do not edit below this line
 module.exports = palindromes;
