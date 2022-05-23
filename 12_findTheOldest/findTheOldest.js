8c568e8e1994038dde96fbf32a6273c37957890e
connorAlex <alex.connor.cochrane@gmail.com>
2022-05-23 15:22:49 -0500

    add 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let max = 0;
+    let oldest = "";
+    arr.forEach(item => {
+        if (typeof(item.yearOfDeath) === 'undefined'){
+            item.yearOfDeath = new Date().getFullYear();
+        }
+        let age = item.yearOfDeath - item.yearOfBirth;
 
+        if (age > max){
+            max = age;
+            oldest = item;
+        }
+        
+    });
+    return oldest;
 };
 
 // Do not edit below this line
